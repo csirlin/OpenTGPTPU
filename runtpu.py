7e9e2d367f93472719711635cb3f102bbffbf41a
csirlin <csirlin@ucsb.edu>
2024-06-06 02:09:10 -0700

    fixing duplicate code

--- runtpu.py
+++ runtpu.py
@@ -205,14 +205,6 @@ while True:
     # for i in range(len(acc_mems)):
     #     print(f"\t{i}: {sim.inspect_mem(acc_mems[i])}")
     # print()
-    # if 314 < cycle < 318:
-    #     print(f"UBuffer@{cycle}: {sim.inspect_mem(UBuffer)}")
-    #     print(f"AccMems[0]@{cycle}: {sim.inspect_mem(acc_mems[0])}")
-    # print(f"UBuffer@{cycle}: {sim.inspect_mem(UBuffer)}")
-    # print(f"AccMems@{cycle}:")
-    # for i in range(len(acc_mems)):
-    #     print(f"\t{i}: {sim.inspect_mem(acc_mems[i])}")
-    # print()
     sim.step(d)
     cycle += 1
 
