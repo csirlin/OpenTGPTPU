63cb77044c55e9d559d725cf01906c954fd492e2
Colter Sirlin <csirlin@ucsb.edu>
2024-12-16 20:55:03 -0800

    clean activate.py

--- activate.py
+++ activate.py
@@ -32,50 +32,20 @@ def act_top(pc, acc_mems, start, start_addr, dest_addr, nvecs, func, accum_out, 
     N_wv = WireVector(len(nvecs), "act_N_wv")
     N_wv <<= N
     act_func = Register(len(func), "act_func")
-    start_acc_mems = []
-    for i in range(matsize):
-        start_acc_mems.append(Register(32, f"act_start_acc_mems_{i}"))
+
     start_addr_reg = Register(len(start_addr), 'act_start_addr_reg')
     start_addr_wv = WireVector(len(start_addr), 'act_start_addr_wv')
     start_addr_wv <<= start_addr_reg
-    
-    for i in range(len(accum_out)):
-        act_top_accum_out_i = WireVector(len(accum_out[i]), f"act_top_accum_out_{i}")
-        act_top_accum_out_i <<= accum_out[i]
 
     rtl_assert(~(start & busy), Exception("Dispatching new activate instruction while previous instruction is still running."))
 
-    for i in range(len(start_acc_mems)):
-        start_acc_mems[i].next <<= select(start, accum_out[i], start_acc_mems[i])
-
-    acc_mems_wvs = [WireVector(len(acc_mems[i][start_addr_reg]), f"act_acc_mems_wv_{i}") for i in range(len(acc_mems))]
     acc_outs_wvs = [WireVector(len(accum_out[i]), f"act_acc_outs_wv_{i}") for i in range(len(accum_out))]
-    # print(acc_mems_wvs)
-    # print(acc_outs_wvs)
-    for i in range(len(acc_mems)):
-        acc_mems_wvs[i] <<= acc_mems[i][start_addr_reg]
     for i in range(len(accum_out)):
         acc_outs_wvs[i] <<= accum_out[i]
 
     start_addr_reg.next <<= select(start, start_addr, start_addr_reg)
-    branch_enable = WireVector(32, "act_branch_enable")
-    branch_enable <<= (acc_mems[-2][start_addr_reg])
-    top_left = WireVector(32, "act_top_left")
-    top_left <<= acc_mems[0][start_addr_reg]
-    one_wv = WireVector(len(nvecs), "act_one_wv")
-    one_wv <<= 1
-    # cond = WireVector(1, "act_cond") # old
-    # cond <<= busy & (N_wv == 1) & (branch_enable == 1) & (top_left == 0) # old
-    pc_incr = WireVector(len(pc), "act_pc_incr") # keep
-    # pc_incr <<= acc_mems[-3][start_addr_reg] # old
 
-    # pc_incr <<= concat_list([acc_mems[-3][start_addr_reg][0:8], acc_mems[-4][start_addr_reg][0:4]]) # WireVector(len(pc))
-    # pc_diff_high = WireVector(len(acc_mems[-4][start_addr_reg]), "act_pc_diff_high")
-    # pc_diff_high <<= acc_mems[-4][start_addr_reg]
-    # pc_diff_low = WireVector(len(acc_mems[-3][start_addr_reg]), "act_pc_diff_low")
-    # pc_diff_low <<= acc_mems[-3][start_addr_reg]
-    # pc_incr[0:8] <<= acc_mems[-3][start_addr_reg]
-    # pc_incr[8:] <<= acc_mems[-4][start_addr_reg][0:len(pc)-8]
+    pc_incr = WireVector(len(pc), "act_pc_incr") # keep
 
     # general condition to take action:
     cond = WireVector(1, "act_cond") # new
@@ -84,11 +54,6 @@ def act_top(pc, acc_mems, start, start_addr, dest_addr, nvecs, func, accum_out, 
     accum_mod = [WireVector(len(accum_out[i]), f"act_accum_mod_{i}") for i in range(len(accum_out))]
     for i in range(len(accum_out)):
         accum_out[i].name = f'act_accum_out_{i}'
-
-    acc_mems_at_start_addr_reg = [None for i in range(len(acc_mems))]
-    for i in range(len(acc_mems)):
-        acc_mems_at_start_addr_reg[i] = WireVector(len(acc_mems[i][start_addr_reg]), f"act_acc_mems_at_start_addr_reg_{i}")
-        acc_mems_at_start_addr_reg[i] <<= acc_mems[i][start_addr_reg]
 
     first_cycle = Register(1, "act_first_cycle") # true for the first busy activation cycle, false otherwise
     pc_incr_reg = Register(len(pc), "act_pc_incr_reg") # hold the pc jump obtained in this cycle for future cycles
@@ -184,7 +149,6 @@ def act_top(pc, acc_mems, start, start_addr, dest_addr, nvecs, func, accum_out, 
     act_out = mux(act_func, invals, relu_vector(accum_mod, 24), 
                   sigmoid_vector(accum_mod), invals) # relu might not work with 32-bit DWIDTH as represented currently. 
     act_out.name = 'act_out'
-    #act_out = relu_vector(accum_out, 24)
     ub_we = busy
             
     return accum_addr, ub_waddr, act_out, ub_we, busy
