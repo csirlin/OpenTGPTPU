135dc74c4045e553ff918c386042b6b1308e7d13
Colter Sirlin <csirlin@ucsb.edu>
2025-02-20 13:12:50 -0800

    revise view_hostmem to use the ProgramType enum

--- test/squish/view_hostmem.py
+++ test/squish/view_hostmem.py
@@ -1,22 +1,26 @@
 import sys
 import os
 import pickle
+from squishtest import ProgramType
 
 # add base folder (OPENTGPTPU) to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 import runtpu
 
-test_name = "full_test_100d_all_matsizes"
-test_type = "rw_mmc_empty_no_s"
-control = False
+# test_name = "full_test_100d_small_matsizes"
+test_name = "full_test_nonop_attempt_2"
+test_type = "mmc_rw_one_space_no_s"
+program_type = ProgramType.NoNop
 bitwidth = 32
 matsize = 4
-distance = 3
+distance = 100
 
-if control:
+if program_type == ProgramType.Control:
     file_name = f"test/squish/{test_name}/{test_type}/ctrl_{bitwidth}b_{matsize}m/hostmem.pkl"
-else:
+elif program_type == ProgramType.Test:
     file_name = f"test/squish/{test_name}/{test_type}/test_{bitwidth}b_{matsize}m_{distance}d/hostmem.pkl"
+elif program_type == ProgramType.NoNop:
+    file_name = f"test/squish/{test_name}/{test_type}/nonop_{bitwidth}b_{matsize}m/hostmem.pkl"
 hostmem = pickle.load(open(file_name, "rb"))
 
 print(f"Hostmem for {file_name}:")
