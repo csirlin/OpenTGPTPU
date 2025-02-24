986960ca5e5e89ce4cc68c4a3072efeb0ba1a825
Colter Sirlin <csirlin@ucsb.edu>
2025-02-23 16:02:33 -0800

    update view_mems code for new memory saving format

--- test/squish/view_mems.py
+++ test/squish/view_mems.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import pickle
+
+import numpy as np
 from squishtest import ProgramType
 
 # add base folder (OPENTGPTPU) to sys.path
@@ -8,21 +10,25 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")
 import runtpu
 
 # test_name = "full_test_100d_small_matsizes"
-test_name = "full_test_nonop_attempt_2"
-test_type = "mmc_rw_one_space_no_s"
-program_type = ProgramType.NoNop
+test_name = "small_matsizes_50d_feb22"
+test_type = "mmc_rw_full_yes_s"
+program_type = ProgramType.Test
 bitwidth = 32
 matsize = 4
-distance = 100
+distance = 14
+DEVICE_TYPE = "runtpu" # "runtpu" or "sim"
 
-if program_type == ProgramType.Control:
-    file_name = f"test/squish/{test_name}/{test_type}/ctrl_{bitwidth}b_{matsize}m/hostmem.pkl"
-elif program_type == ProgramType.Test:
-    file_name = f"test/squish/{test_name}/{test_type}/test_{bitwidth}b_{matsize}m_{distance}d/hostmem.pkl"
-elif program_type == ProgramType.NoNop:
-    file_name = f"test/squish/{test_name}/{test_type}/nonop_{bitwidth}b_{matsize}m/hostmem.pkl"
-hostmem = pickle.load(open(file_name, "rb"))
+def get_file_name(file_name, test_name, test_type, program_type, bitwidth, matsize, distance):
+    if program_type == ProgramType.Control:
+        return f"test/squish/{test_name}/{test_type}/ctrl_{bitwidth}b_{matsize}m/{DEVICE_TYPE}_{file_name}.npy"
+    elif program_type == ProgramType.Test:
+        return f"test/squish/{test_name}/{test_type}/test_{bitwidth}b_{matsize}m_{distance}d/{DEVICE_TYPE}_{file_name}.npy"
+    elif program_type == ProgramType.NoNop:
+        return f"test/squish/{test_name}/{test_type}/nonop_{bitwidth}b_{matsize}m/{DEVICE_TYPE}_{file_name}.npy"
 
-print(f"Hostmem for {file_name}:")
-runtpu.print_mem(hostmem, bits=bitwidth, size=matsize)
-print(f"Hostmem for {file_name}:")
+
+for memory in ["hostmem", "weightsmem", "ubuffer", "wqueue", "accmems"]:
+    file_name = get_file_name(memory, test_name, test_type, program_type, bitwidth, matsize, distance)
+    loaded_mem = np.load(file_name)
+    print(f"{memory} for {file_name}:")
+    print(loaded_mem)
